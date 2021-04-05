6830f833c040c3518053095302c691bdd6bfd2d3
Jordan Roessle <screamaimfire@me.com>
2021-04-05 16:34:39 -0700

    Added more comments for clarity

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -15,16 +15,20 @@ with open(csvpath) as election_data:
 	#rubric says to save header, I don't use it anywhere else though
 	throwaway_variable = next(csviterator)
 
-	#loop through the dataset 
+	#loop through the dataset
+	#loop creates list for name of each candidate and another list for how many votes they got 
 	for row in csviterator:
 		total_count += 1
+		#if name hasn't been seen before add to name list
 		if(name_list.count(row[2]) == 0):
 			name_list.append(row[2])
 			count_list.append(1)
+		#otherwise add count to number of votes for that name
 		else:
 			index = name_list.index(row[2])
 			count_list[index] += 1
 
+#set up list for output
 output_list = []
 output_list.append("Election Results")
 output_list.append("-------------------------")
@@ -33,10 +37,12 @@ output_list.append("-------------------------")
 
 index = 0
 winner_count = 0
-#for loop to add results per candidate 
+#for loop to add results per candidate by name 
 for name in name_list:
+	#if number of votes is greater than highest seen so far, update it
 	if(count_list[index] > winner_count):
 		winner_count = count_list[index]
+	#add candidate and their number of votes to output list
 	output_list.append(f"{name}: {round(100 * (float(count_list[index]) / (total_count)), 3)}\
 % ({count_list[index]})")
 	index +=1
@@ -48,7 +54,7 @@ output_list.append("-------------------------")
 output_list.append(f"Winner: {winner}")
 output_list.append("-------------------------")
 
-#prepare for output to text file
+#prepare for output to text file and print output
 output_path = os.path.join("analysis", "election_results.txt")
 
 with open(output_path, 'w', newline='') as output_file:
