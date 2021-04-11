92166594bee217e7a55787176bbf3471f6556731
Jordan Roessle <screamaimfire@me.com>
2021-04-10 17:01:47 -0700

    PyParagraph now fully functional, have to change file name in code though

--- ExtraContent/PyParagraph/main.py
+++ ExtraContent/PyParagraph/main.py
@@ -5,7 +5,7 @@ filepath = os.path.join("raw_data", "givenPassage.txt")
 
 with open(filepath) as paragraph:
 	for line in paragraph:
-		#calculate word count 
+		#calculate word count, I do not know how the example gave 122
 		words = line.split(' ')
 		word_count = len(words)
 
@@ -16,21 +16,20 @@ with open(filepath) as paragraph:
 		for word in words:
 			character_count = 0
 			for character in word:
-				if(character.isalpha())
+				#the example doesn't check for alphabet, but it makes more sense to me like this
+				if(character.isalpha()):
 					character_count += 1
 			word_lengths.append(character_count)
+		
+		#calculate average word length
+		average_word_length = sum(word_lengths) / len(word_lengths)
 
+		#calculate average sentence length
+		average_sentence_length = word_count / sentence_count
 
-
-
-
-
-
-
-
-
-
-
-		print(word_count)
-		print(sentence_count)
-		print(word_lengths)
+		print("Paragraph Analysis")
+		print("-----------------")
+		print(f"Approximate Word Count: {word_count}")
+		print(f"Approximate Sentence Count: {sentence_count}")
+		print(f"Average Letter Count: {average_word_length}")
+		print(f"Average Sentence Length: {average_sentence_length}")
